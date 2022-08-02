09cde3c4ba86243d84cf9648a31bd0e87dcf6472
Hugo Developer <hugo212h@gmail.com>
2022-08-02 15:08:34 -0500

    ✨ Funciones Asíncronas

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,81 +1,84 @@
-import weather from '../data/current-weather.js'
-import { formatDate, formatTemp } from './utils/format-data.js';
-import { weatherConditionsCodes } from './constants.js'
-import { getCurrentPosition } from './geolocation.js'
+import weather from "../data/current-weather.js";
+import { formatDate, formatTemp } from "./utils/format-data.js";
+import { weatherConditionsCodes } from "./constants.js";
+import { getLatLon } from "./geolocation.js";
 // weather.main.temp
 // weatherConditionsCodes['2']
 // weatherConditionsCodes[]
 
 function setCurrentCity($el, city) {
-  $el.textContent = city
+  $el.textContent = city;
 }
 
 function setCurrentDate($el) {
-  const date = new Date()
-  const formattedDate = formatDate(date)
-  $el.textContent = formattedDate
+  const date = new Date();
+  const formattedDate = formatDate(date);
+  $el.textContent = formattedDate;
 }
 
 function setCurrentTemp($el, temp) {
-  $el.textContent = formatTemp(temp)
+  $el.textContent = formatTemp(temp);
 }
 
 function solarStatus(sunsetTime, sunriseTime) {
-  const currentHours = new Date().getHours()
-  const sunsetHours = sunsetTime.getHours()
-  const sunriseHours = sunriseTime.getHours()
+  const currentHours = new Date().getHours();
+  const sunsetHours = sunsetTime.getHours();
+  const sunriseHours = sunriseTime.getHours();
 
   if (currentHours > sunsetHours || currentHours < sunriseHours) {
-    return 'night'
+    return "night";
   }
-  return 'morning'
+  return "morning";
 }
 
 function setBackground($el, conditionCode, solarStatus) {
-  const weatherType = weatherConditionsCodes[conditionCode]
-  const size = window.matchMedia('(-webkit-min-device-pixel-ratio: 2)').matches ? '@2x' : ''
+  const weatherType = weatherConditionsCodes[conditionCode];
+  const size = window.matchMedia("(-webkit-min-device-pixel-ratio: 2)").matches ? "@2x" : "";
   // let size =''
   // if (window.matchMedia('(-webkit-min-device-pixel-ratio: 2)').matches) {
   //   size = '@2x'
   // }
 
-  $el.style.backgroundImage = `url(./images/${solarStatus}-${weatherType}${size}.jpg)`
+  $el.style.backgroundImage = `url(./images/${solarStatus}-${weatherType}${size}.jpg)`;
 }
 
 function configCurrentWeather(weather) {
   // Loader
   // Date
-  const $currentWeatherDate = document.querySelector('#current-weather-date')
-  setCurrentDate($currentWeatherDate)
+  const $currentWeatherDate = document.querySelector("#current-weather-date");
+  setCurrentDate($currentWeatherDate);
   // City
-  const $currentWeatherCity = document.querySelector('#current-weather-city')
-  const city = weather.name
-  setCurrentCity($currentWeatherCity, city)
+  const $currentWeatherCity = document.querySelector("#current-weather-city");
+  const city = weather.name;
+  setCurrentCity($currentWeatherCity, city);
   // Temperature
-  const $currentWeatherTemp = document.querySelector('#current-weather-temp')
-  const temp = weather.main.temp
-  setCurrentTemp($currentWeatherTemp, temp)
+  const $currentWeatherTemp = document.querySelector("#current-weather-temp");
+  const temp = weather.main.temp;
+  setCurrentTemp($currentWeatherTemp, temp);
   // Background
-  const sunriseTime = new Date(weather.sys.sunrise * 1000)
-  const sunsetTime = new Date(weather.sys.sunset * 1000)
+  const sunriseTime = new Date(weather.sys.sunrise * 1000);
+  const sunsetTime = new Date(weather.sys.sunset * 1000);
 
-  const $app = document.querySelector('#app')
-  const conditionCode = String(weather.weather[0].id).charAt(0)
-  setBackground($app, conditionCode, solarStatus(sunriseTime, sunsetTime))
+  const $app = document.querySelector("#app");
+  const conditionCode = String(weather.weather[0].id).charAt(0);
+  setBackground($app, conditionCode, solarStatus(sunriseTime, sunsetTime));
 }
 
-export default function currentWeather() {
+export default async function currentWeather() {
   // GEOLOCATION // API - weather // Config
-  console.log('Esto pasa ANTES de getCurrentPosition')
-  // const latlon = getCurrentPosition()
-  getCurrentPosition()
-  .then((data) => {
-    console.log('Hemos triunfado', data)
-  })
-  .catch((message) => {
-    console.log(message)
-  })
-  console.log('Esto pasa DESPUES de getCurrentPosition')
+  console.log("Esto pasa ANTES de getCurrentPosition");
+  const { lat, lon, isError } = await getLatLon();
+  if (isError) return console.log("Ah ocurrido un error al obtener tu ubicación");
+  console.log(lat, lon);
+
+  // getCurrentPosition()
+  // .then((data) => {
+  //   console.log('Hemos triunfado', data)
+  // })
+  // .catch((message) => {
+  //   console.log(message)
+  // })
+  // console.log('Esto pasa DESPUES de getCurrentPosition')
   configCurrentWeather(weather)
-  console.log(weather)
+  // console.log(weather)
 }
--- js/geolocation.js
+++ js/geolocation.js
@@ -6,18 +6,39 @@ function geolocationSupport() {
   return "geolocation" in navigator;
 }
 
-export function getCurrentPosition() {
+const defaultOptions = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 1000000,
+};
+
+export function getCurrentPosition(options = defaultOptions) {
   if (!geolocationSupport()) throw new Error("No hay soporte de Geolocalización en tu navegador");
 
   return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-      resolve({ lat, lon });
-      console.log(lat, lon);
-      console.log("Esto ES getCurrentPosition");
-    },()=>{
-      reject("No se pudo obtener tu ubicación");
-    },{})
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        resolve(position);
+        // console.log(lat, lon);
+        // console.log("Esto ES getCurrentPosition");
+      },
+      () => {
+        reject("No se pudo obtener tu ubicación");
+      },
+      options
+    );
   });
+}
+
+export async function getLatLon(options = defaultOptions) {
+  try {
+    const {
+      coords: { latitude: lat, longitude: lon },
+    } = await getCurrentPosition(options);
+    return { lat, lon, isError: false };
+  } catch {
+    return { isError: true, lat: null, lon: null };
+  }
 }
