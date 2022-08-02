8ad19306e349baf3e1e212da214801b5a317ea2d
Hugo Developer <hugo212h@gmail.com>
2022-08-02 02:02:30 -0500

    ✨ Añadiendo JS, creando el modulo del clima actual

---
+++ js/current-weather.js
@@ -0,0 +1,15 @@
+import weather from '../data/current-weather.js'
+
+function configCurrentWeather(weather) {
+  // Loader
+  // Date
+  // City
+  // Temperature
+  // Background
+}
+
+export default function currentWeather() {
+  // GEOLOCATION // API - weather // Config
+  configCurrentWeather(weather)
+  console.log(weather)
+}
