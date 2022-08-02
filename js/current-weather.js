9c97f6384c8ca0efc547c7d481157c30613f7da2
Hugo Developer <hugo212h@gmail.com>
2022-08-02 11:15:35 -0500

    ✨ Background Dinámico

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,5 +1,6 @@
 import weather from '../data/current-weather.js'
 import { formatDate, formatTemp } from './utils/format-data.js';
+// weather.main.temp
 
 function setCurrentCity($el, city) {
   $el.textContent = city
@@ -13,6 +14,21 @@ function setCurrentDate($el) {
 
 function setCurrentTemp($el, temp) {
   $el.textContent = formatTemp(temp)
+}
+
+function solarStatus(sunsetTime, sunriseTime) {
+  const currentHours = new Date().getHours()
+  const sunsetHours = sunsetTime.getHours()
+  const sunriseHours = sunriseTime.getHours()
+
+  if (currentHours > sunsetHours || currentHours < sunriseHours) {
+    return 'night'
+  }
+  return 'morning'
+}
+
+function setBackground($el, solarStatus) {
+  $el.style.backgroundImage = `url(./images/${solarStatus}-drizzle.jpg)`
 }
 
 function configCurrentWeather(weather) {
@@ -29,6 +45,11 @@ function configCurrentWeather(weather) {
   const temp = weather.main.temp
   setCurrentTemp($currentWeatherTemp, temp)
   // Background
+  const sunriseTime = new Date(weather.sys.sunrise * 1000)
+  const sunsetTime = new Date(weather.sys.sunset * 1000)
+
+  const $app = document.querySelector('#app')
+  setBackground($app, solarStatus(sunriseTime, sunsetTime))
 }
 
 export default function currentWeather() {
